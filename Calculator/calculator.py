5acf106cee1eba7479e7108306a871e8a83a5461
RIYA KUMARI <77605628+Riyakumari-ops-coder@users.noreply.github.com>
2021-10-02 01:35:54 +0530

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -16,14 +16,7 @@ calc.grid()
 #*************************************************************************************#
 
 
-class Calc():
-    def __init__(self):
-        self.total = 0
-        self.current = ' '
-        self.input_value = True
-        self.check_sum = False
-        self.op = ''
-        self.result = False
+
 
     def numberEnter(self, num):
         self.result = False
